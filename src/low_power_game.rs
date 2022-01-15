7042363e9909b308ad77d6bd07901a9ba2ad474d
silence_zhpf <silence_zhpf@aliyun.com>
2022-01-15 09:18:21 +0800

    feat(low_power_game): ✨ 添加low_power_game模块
    ✨ 添加low_power_game模块
    
    silence_zhpf<silence_zhpf@aliyun.com>

---
+++ src/low_power_game.rs
@@ -0,0 +1,37 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_div_mod() {
+        assert_eq!(div_mod(10, 3), (3, 1))
+    }
+
+    #[test]
+    fn test_evens_int() {
+        let mut even_ints = evens(0_u8..);
+        assert_eq!(even_ints.next(), Some(0));
+        assert_eq!(even_ints.next(), Some(2));
+        assert_eq!(even_ints.next(), Some(4));
+        assert_eq!(even_ints.next(), Some(6));
+    }
+
+    #[test]
+    fn test_evens_from_odds() {
+        let mut evens_from_odds = evens(1_i16..);
+        assert_eq!(evens_from_odds.next(), Some(1));
+        assert_eq!(evens_from_odds.next(), Some(3));
+        assert_eq!(evens_from_odds.next(), Some(5));
+        assert_eq!(evens_from_odds.next(), Some(7));
+    }
+}
+
+fn div_mod(dividend: i16, divisor: i16) -> (i16, i16) {
+    (dividend / divisor, dividend % divisor)
+}
+
+fn evens<T>(iter: impl Iterator<Item=T>) -> impl Iterator<Item=T> {
+    iter.enumerate()
+        .filter(|(i, _)| i % 2 == 0)
+        .map(|(_, v)| v)
+}
