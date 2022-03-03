4f6ff8880d7b8cf9ad77b028285acb576f50c244
silence_zhpf <silence_zhpf@aliyun.com>
2022-03-03 23:27:32 +0800

    feat(rpn_calculator): ✨ 实现模拟计算器
    silence_zhpf<silence_zhpf@aliyun.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod health_statistic;
 mod low_power_game;
 mod resistor_color;
 mod role_playing_game;
+mod rpn_calculator;
 mod short_fibonacci_sequence;
 mod struct_logs;
 
---
+++ src/rpn_calculator.rs
@@ -0,0 +1,57 @@
+use std::ptr::null;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn simple_inputs() {
+        let inputs = [
+            CalculatorInput::Value(2),
+            CalculatorInput::Value(3),
+            CalculatorInput::Add,
+        ];
+        let result = evaluate(&inputs);
+        assert_eq!(result, Option::Some(5));
+    }
+}
+
+#[derive(Debug)]
+pub enum CalculatorInput {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Value(i32),
+}
+
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    let mut number_stack: Vec<i32> = Vec::new();
+    let len = inputs.len();
+    for i in 0..len {
+        let input = inputs.get(i);
+        match input {
+            Some(in_input) => match in_input {
+                CalculatorInput::Value(j) => number_stack.push(*j),
+                CalculatorInput::Add => {
+                    let x = number_stack.pop().expect("") + number_stack.pop().expect("");
+                    number_stack.push(x)
+                }
+                CalculatorInput::Subtract => {
+                    let x = number_stack.pop().expect("") + number_stack.pop().expect("");
+                    number_stack.push(x)
+                }
+                CalculatorInput::Divide => {
+                    let x = number_stack.pop().expect("") / number_stack.pop().expect("");
+                    number_stack.push(x)
+                }
+                CalculatorInput::Multiply => {
+                    let x = number_stack.pop().expect("") * number_stack.pop().expect("");
+                    number_stack.push(x)
+                }
+            },
+            None => (),
+        }
+    }
+    return number_stack.pop();
+}
